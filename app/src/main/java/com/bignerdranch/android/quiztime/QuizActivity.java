66f3fa65ba642f95b7487d6f895822b45b764beb
satishc2938 <satishc2938@gmail.com>
2016-03-30 01:16:57 +0530

    Adding 2 button members

--- app/src/main/java/com/bignerdranch/android/quiztime/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/quiztime/QuizActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
